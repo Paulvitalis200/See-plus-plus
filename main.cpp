710ef6342bc5d5fb12b972199a592fbaa387157d
Paulstar200 <vitalispaul48@live.com>
2020-10-10 20:52:30 +0300

    Basic application printing

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName = "Tom";
+    int characterAge;
+
+    characterAge = 33;
+    cout << "There once was a man named " << characterName << endl;
+    cout << "He was " << characterAge << " years old" << endl;
+    cout << "He liked the name George" << endl;
+
+    characterAge = 59;
+    cout << "But did not like being " << characterAge << endl;
+    return 0;
+}

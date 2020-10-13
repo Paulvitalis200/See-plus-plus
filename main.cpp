bc84edd32bc08bdd8b38f2d365cad9818ec960b8
Paulstar200 <vitalispaul48@live.com>
2020-10-13 13:52:46 +0300

    Function stubs

--- main.cpp
+++ main.cpp
@@ -3,14 +3,17 @@
 
 using namespace std;
 
-
-void sayHi(string name, int age) {
-    cout << "Hello " << name << ", you are " << age <<endl;
-}
+// Function stub/function signature
+void sayHi(string name, int age);
 
 int main()
 {
     sayHi("Mike", 59);
     sayHi("Paul", 29);
     return 0;
+}
+
+
+void sayHi(string name, int age) {
+    cout << "Hello " << name << ", you are " << age <<endl;
 }

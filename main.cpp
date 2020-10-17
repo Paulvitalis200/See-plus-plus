5823b4d5652fd2a4987668b7d7736ad19df7ab69
Paulstar200 <vitalispaul48@live.com>
2020-10-17 19:43:41 +0300

    Guessing game

--- main.cpp
+++ main.cpp
@@ -5,10 +5,27 @@ using namespace std;
 
 int main()
 {
-    int count = 1;
-    while(count < 5) {
-        cout << "Hello" << endl;
-        count++;
+    int secretNum = 7;
+    int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    bool outOfGuesses = false;
+
+    while (guess != secretNum && !outOfGuesses) {
+        if (guessCount < guessLimit) {
+            cout << "Enter guess: ";
+            cin >> guess;
+            guessCount++;
+        } else {
+            outOfGuesses = true;
+        }
     }
+
+    if (outOfGuesses) {
+        cout << "You lose!" <<endl;
+    } else {
+        cout << "You Win!" <<endl;
+    }
+    
     return 0;
 }

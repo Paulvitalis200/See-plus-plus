fd27831d80d3664bdab6984269041df949d0b9ae
Paulstar200 <vitalispaul48@live.com>
2020-10-17 19:25:02 +0300

    Switch statements

--- main.cpp
+++ main.cpp
@@ -3,17 +3,42 @@
 
 using namespace std;
 
+string getDayOfWeek(int dayNum) {
+    string dayName;
+
+    switch(dayNum) {
+        case 0:
+            dayName = "Sunday";
+            break;
+        case 1:
+            dayName = "Monday";
+            break;
+        case 2:
+            dayName = "Tuesday";
+            break;
+        case 3:
+            dayName = "Wednesday";
+            break;
+        case 4:
+            dayName = "Thursday";
+            break;
+        case 5:
+            dayName = "Saturday";
+            break;
+        case 6:
+            dayName = "Sunday";
+            break;
+        default:
+            dayName = "Invalid day number";
+
+    }
+    return dayName;
+}
+
 int main()
 {
-    bool isMale = true;
-    bool isTall = true;
-    if(!isMale || !isTall){
-        cout << "Paul" << endl;
-    } else if (isMale && isTall) {
-         cout << "Naaaa" << endl;
-    }
-    else {
-        cout << "You are not male" << endl;
-    }
+    string day = getDayOfWeek(10);
+
+    cout << day << endl;
     return 0;
 }

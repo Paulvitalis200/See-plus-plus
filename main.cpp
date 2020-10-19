b5c8aef044cb4a7e6cd535f9665482df133c6cc5
Paulstar200 <vitalispaul48@live.com>
2020-10-19 23:18:13 +0300

    Comments

--- main.cpp
+++ main.cpp
@@ -5,14 +5,10 @@ using namespace std;
 
 int main()
 {
-    int numberGrid[3][2] = {
-        {1, 2}, {3, 4}, {5, 6}
-    };
-
-    for(int i = 0; i < 3; i++) {
-        for (int j = 0; j < 2; j++) {
-            cout << numberGrid[i][j] << endl;
-        }
-    }
+    // This is a comment
+    /*
+    This is a multi line comment
+    */
+    cout << "Comments are fun";
     return 0;
 }

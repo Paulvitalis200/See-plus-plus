64ff95dbd1df7324b3f803233b0fe8911a989535
Paulstar200 <vitalispaul48@live.com>
2020-10-19 23:06:16 +0300

    Using a for loop to create an exponent function

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 
 using namespace std;
 
+int power(int baseNum, int powNum) {
+    int result = 1;
+
+    for(int i = 0; i < powNum; i++) {
+        result = result * baseNum;
+    }
+
+    return result;
+}
+
 int main()
 {
-    int nums[] = {1, 2, 3, 4, 5};
-    for(int i = 0; i < 5; i++) {
-       cout << nums[i] << endl;
-    }
-    
+    cout << power(4, 2) << endl;
     return 0;
 }

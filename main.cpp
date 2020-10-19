0465525c3f94d7183788d79f412f1a1b86e592dc
Paulstar200 <vitalispaul48@live.com>
2020-10-19 23:14:24 +0300

    Loop two dimensional arrays

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@ int main()
         {1, 2}, {3, 4}, {5, 6}
     };
 
-    cout << numberGrid[0][1] << endl;
+    for(int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            cout << numberGrid[i][j] << endl;
+        }
+    }
     return 0;
 }
